a9f254171f32973f85b11b5532ae0f6d43e041b6
Devan Thakur <devanthakur89@gmail.com>
2024-07-27 23:19:16 +0530

    Updates hamburger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,11 +19,7 @@ module.exports = {
       secondaryBG: '#1A1B2A',
       primaryText: '#141035',
       secondaryText: '#5E5E5E',
-      tertiaryText: '#CBCCCE',
-      btnPrimary: '#5D5FEF',
-      btnSecondary: 'rgba(93, 95, 239, 0.20)',
-      'white-alpha-20': 'rgba(255, 255, 255, 0.2)',
-      'white-alpha-25': 'rgba(255, 255, 255, 0.25)'
+      tertiaryText: '#CBCCCE'
     },
     extend: {},
   },
